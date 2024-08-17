c925367874cf259f6af0228ef1075bfc0b02e1a1
NickFeD <66272332+NickFeD@users.noreply.github.com>
2024-08-17 21:11:40 +0200

    add WebSoketController

---
+++ MessageExchange/Controllers/WebSocketController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MessageExchange.Controllers;
+
+public class WebSocketController:ControllerBase
+{
+    [Route("/ws")]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            //await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+}
